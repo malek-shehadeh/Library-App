9701fff2426677c262b3cfb7a38becf71e0f2b96
malekshehadeh <malek.shehadeh96@gmail.com>
2024-07-09 21:47:21 +0300

    Final version

---
+++ library-app/src/Object.js
@@ -0,0 +1,29 @@
+const initState = {
+  books: [
+    {
+      id: 1,
+      title: "مقدمة ابن خلدون",
+      author: "ابن خلدون",
+      isbn: "1289499030",
+    },
+    {
+      id: 2,
+      title: "الحاوي في الطب",
+      author: "ابو بكر الرازي",
+      isbn: "893847239479",
+    },
+    {
+      id: 3,
+      title: "الشفاء",
+      author: "ابن سينا",
+      isbn: "1234567890",
+    },
+    {
+      id: 4,
+      title: "تاريخ الأمم والملوك",
+      author: "الطبري",
+      isbn: "0987654321",
+    },
+  ],
+};
+export default initState;
